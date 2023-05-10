56b6e2b7e30559e887b3bcda4b0376ae66d171b6
bcrudele <120442376+bcrudele@users.noreply.github.com>
2023-05-10 03:22:27 -0400

    Excel Output Support
    Beautified terminal and now generates Excel output file. Made code more modular with 'deviation' and '# results' inputs. Currently utilizes a static array to hold player and goalie ratings.

--- team_allocation.m
+++ team_allocation.m
@@ -9,94 +9,150 @@ fprintf('==============================\n\n');
 min_ppt = 3; % Minimum allowed skaters per team
 min_teams = 2; % Minimum teams allowed in league
 players_per_team = 0; % Ratio of players per team requested (subject to UI)
-num_teams = -1; % Number of teams requested
-num_players = -1; % Number of skaters (DNI GOALIES)
-
-players = {'x', 'y', 'z', 'p', 'q', 'u'};
-    % 'Name8', 'Name9', 'Name10', 'Name11', 'Name12', 'Name13', 'Name14', 'Name15'};
-ratings = [10 4 6 7 8 8];
-goalies = {'Goalie1', 'Goalie2', 'Goalie3', 'Goalie4'};
+players = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'}; % 16 char
+ratings = [10    9    8    7    6    5    4    3    2    1    1    2    3    4    5    6];
+goalies = {'G1', 'G2', 'G3', 'G4'};
+gratings = [10    1     5     5];        % Goalie ratings
 team_names = {'Chicago Charge', 'Midwest Melkmen', 'The Disappointments', ...
     'Toxic Turtles'};
 
 %% User Input
+while (players_per_team < min_ppt || num_teams < min_teams || ... 
+        num_teams > length(goalies) ||length(players)/num_teams < players_per_team)
 
-while ((mod(num_players,1) ~= 0) || (num_players <= 0) )
-    fprintf("Players Available: ")
-    num_players = errorHandler(num_players);
-end
+    num_teams = -1; % Number of teams requested
+    num_players = -1; % Number of skaters (DNI GOALIES)
+    input_dev = -1; % Input deviation request
+    num_outs = -1; % Number outputs requested
 
-while ((mod(num_teams,1) ~= 0) || (num_teams <= 0) )
-    fprintf("Teams Requested: ")
-    num_teams = errorHandler(num_teams);
-end
+    while ((mod(num_players,1) ~= 0) || (num_players <= 0) )
+        fprintf("Players Available: ")
+        num_players = errorHandler(num_players);
+    end
 
-%% Check validity
+    while ((mod(num_teams,1) ~= 0) || (num_teams <= 0) )
+        fprintf("Teams Requested: ")
+        num_teams = errorHandler(num_teams);
+    end
 
-players_per_team = num_players/num_teams;
+    while ((mod(input_dev,1) ~= 0) || (input_dev <= 0) )
+        fprintf("Maximum Skill Deviation: ")
+        input_dev = errorHandler(input_dev);
+    end
 
-if players_per_team < min_ppt
-    fprintf("Minimum players per team requirement not met. \n")
+    while ((mod(num_outs,1) ~= 0) || (num_outs <= 0) )
+        fprintf("# of Combinations Requested: ")
+        num_outs = errorHandler(num_outs);
+    end
 
-elseif num_teams < min_teams
-    fprintf("Not enough teams requested. Current league minimum -> %.f\n", min_teams);
+    players_per_team = num_players/num_teams; 
 
-elseif num_teams > length(goalies)
-    fprintf("Not enough goalies to fufill team number request\n")
+    if players_per_team < min_ppt
+        fprintf("Minimum players per team requirement not met. \n")
 
-elseif ((length(players)/num_teams) < players_per_team)
-    fprintf("Not enough skaters to fufill team number request\n")
+    elseif num_teams < min_teams
+        fprintf("Not enough teams requested. Current league minimum -> %.f\n", min_teams);
+
+    elseif num_teams > length(goalies)
+        fprintf("Not enough goalies to fufill team number request\n")
+
+    elseif ((length(players)/num_teams) < players_per_team)
+        fprintf("Not enough skaters to fufill team number request\n")
+    end
 end
 
 %% Shuffle Function
 
-tot = zeros(1,num_teams);
-random_alloc = [];  % To be shuffled indexing vector
+filecell = cell(((num_outs * num_teams) + 1),(floor(players_per_team)) + 3);
+filecell{1,1} = 'Result #';
+filecell{1,2} = 'Team';
+filecell{1,3} = 'Team Skill';
+filecell{1,4} = 'Deviation';
+filecell{1,5} = 'Goalie';
 
-values = 1:num_players;
-
-% Shuffle the vector of values using the randperm function
-shuffled_values = randperm(num_players);
-
-% Loop over the shuffled values and select each value once
-for i = 1:num_players
-    % Select the next shuffled value
-    selected_value = values(shuffled_values(i));
-    random_alloc(i) = selected_value;
+for k = 6:(5 + floor(players_per_team))
+    filecell{1,k} = 'Player';
 end
 
-ct = 0;
+for k = 1:num_outs % num_outs
 
-%% Output & Loading
+    tot = 0:999:num_teams*999;
 
-if ~((players_per_team < min_ppt) || (num_teams < min_teams) || ...
-        (num_teams > length(goalies)) || ...
-        ((length(players)/num_teams) < players_per_team) )
+    while (std(tot) >= input_dev)
 
-    for i = 1:num_teams
+        tot = zeros(1,num_teams);
+        random_alloc = [];  % To be shuffled indexing vector
 
-        fprintf("\nTeam %s\n\n", team_names{i});
+        values = 1:num_players;
 
-        fprintf("Goalie: %s, \n",goalies{i});
+        % Shuffle the vector of values using the randperm function
+        shuffled_values = randperm(num_players);
+
+        % Loop over the shuffled values and select each value once
+        for i = 1:num_players
+            selected_value = values(shuffled_values(i));
+            random_alloc(i) = selected_value;
+        end
+
+        ct = 0;
+
+        for i = 1:num_teams
+
+            for j = 1:players_per_team
+
+                ct = ct + 1;
+
+                tot(i) = tot(i) + ratings(random_alloc(ct)); % add player rating
+
+            end
+
+            tot(i) = tot(i) + gratings(i); % add goalie rating
+
+        end
+
+    end
+
+    %% Output & Loading
+
+    ct = 0;
+
+    fprintf("  Result #%.f ======================== \n\n", k)
+
+    for z = 1:num_teams
+
+        filecell{((k - 1) * num_teams) + z + 1, 1} = k;
+
+        fprintf("%s skill: %.f\n",team_names{z}, tot(z))
+
+        filecell{((k - 1) * num_teams) + z + 1, 2} = team_names{z};
+
+        filecell{((k - 1) * num_teams) + z + 1, 3} = tot(z);
+
+        fprintf("Goalie: %s, \n",goalies{z});
+
+        filecell{((k - 1) * num_teams) + z + 1, 5} = goalies{z};
 
         for j = 1:players_per_team
 
             ct = ct + 1;
 
+            filecell{((k - 1) * num_teams) + z + 1, 5 + j } ...
+                = players{random_alloc(ct)}; % 5 col. for pre-entries
+
             fprintf("%s, ",players{random_alloc(ct)});
-          
-            tot(i) = tot(i) + ratings(random_alloc(ct));
-            fprintf("\n")
 
         end
+
+        fprintf("\n\n");
+
     end
 
-    if ((num_players - ct) >= 1)
+    filecell{((k - 1) * num_teams) + z + 1, 4} = std(tot);
 
-        fprintf("\n\nBenched Players\n");
-        for i = 1:(num_players - ct)
+    fprintf("Stdev -> %0.5f\n", std(tot))
 
-            fprintf("%s,\n",players{random_alloc(i+ct)})
-        end
-    end
+    fprintf("\n")
+
 end
+
+xlswrite('output.xlsx', filecell);  % Save filecell as an Excel file
